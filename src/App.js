a0f37bbfda41487fec29abaf8fbfe55a7fc1ef5a
Rodrigo <rcoto@protonmail.com>
2019-05-13 20:26:25 -0700

    Feature: Search form completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import SearchLocation from "./components/SearchLocation";
 
 function App() {
-  return <div className="App">Weather Vacation Planner</div>;
+  return (
+    <div className="App">
+      <h1 className="App-title">Decide where your next vacation will be.</h1>
+      <h4 className="App-title-sub">
+        Just pick the date you are planning to travel and we will show you how
+        the weather was the past 3 years the same date.
+      </h4>
+      <SearchLocation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SearchLocation.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+export class SearchLocation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    alert(this.state.value);
+  }
+
+  render() {
+    return (
+      <form className="SearchLocation" onSubmit={this.handleSubmit}>
+        <label htmlFor="location" className="SearchLocation-label">
+          Search Location
+        </label>
+        <input
+          type="text"
+          id="location"
+          className="SearchLocation-input"
+          placeholder="Address or Location"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button className="SearchLocation-button">Get Location</button>
+      </form>
+    );
+  }
+}
+
+export default SearchLocation;
