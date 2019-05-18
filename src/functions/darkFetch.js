6a8ed61712896218678921d3c345bf020c7028cb
Rodrigo <rcoto@protonmail.com>
2019-05-17 22:53:24 -0700

    fixed: App no longer crashes if a remote location doesnt return weather data

--- src/functions/darkFetch.js
+++ src/functions/darkFetch.js
@@ -18,20 +18,32 @@ export const darkFetch = async (lat, lng, time = "") => {
 // Build the object to be ready to store in state
 export const darkBuildData = async (lat, lng, time = "") => {
   const originalResponse = await darkFetch(lat, lng, time);
-  const currentTemp = originalResponse.currently.temperature || false;
-  const weatherCast =
-    originalResponse.daily.data[0] || originalResponse.hourly.data[0];
+  try {
+    const currentTemp = originalResponse.currently.temperature || false;
+    const weatherCast =
+      originalResponse.daily.data[0] || originalResponse.hourly.data[0];
 
-  console.log("Done");
+    console.log("Done");
 
-  const weatherData = {
-    id: uuid(),
-    summary: weatherCast.summary,
-    icon: weatherCast.icon,
-    high: weatherCast.temperatureHigh,
-    low: weatherCast.temperatureLow,
-    current: currentTemp || weatherCast.temperature
-  };
-
-  return weatherData;
+    const weatherData = {
+      id: uuid(),
+      summary: weatherCast.summary,
+      icon: weatherCast.icon,
+      high: weatherCast.temperatureHigh,
+      low: weatherCast.temperatureLow,
+      current: currentTemp || weatherCast.temperature
+    };
+    return weatherData;
+  } catch (error) {
+    console.log("Failed: ", error);
+    const weatherData = {
+      id: uuid(),
+      summary: undefined,
+      icon: undefined,
+      high: undefined,
+      low: undefined,
+      current: undefined
+    };
+    return weatherData;
+  }
 };
