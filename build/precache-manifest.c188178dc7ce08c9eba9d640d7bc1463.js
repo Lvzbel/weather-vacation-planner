59f910fd56fdff46bced744a03bee35c09757fbb
Rodrigo <rcoto@protonmail.com>
2019-05-21 23:45:31 -0700

    Build Website ready to github pages

---
+++ build/precache-manifest.c188178dc7ce08c9eba9d640d7bc1463.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b3477a50f736c6423c622c58025a72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a406a30b185cdfe20bf1",
+    "url": "/static/css/2.c987eb7f.chunk.css"
+  },
+  {
+    "revision": "a211cfe85c175770af15",
+    "url": "/static/css/main.58bb26c1.chunk.css"
+  },
+  {
+    "revision": "a406a30b185cdfe20bf1",
+    "url": "/static/js/2.cc033533.chunk.js"
+  },
+  {
+    "revision": "a211cfe85c175770af15",
+    "url": "/static/js/main.2f1caaf3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
