f62daf947334521199b99e5e0ec14d56944fc6b0
Rodrigo <rcoto@protonmail.com>
2019-05-25 00:30:26 -0700

    Fixed: express is properly displaying the react app

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const DARKSKY_API_KEY = process.env.DARK_SKY_API;
 const axios = require("axios");
 const path = require("path");
 
-router.use(express.static(path.join(__dirname, "react-frontend/build")));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "react-frontend/build")));
 
 router.post("/weather", (req, res) => {
   const params = req.body;
