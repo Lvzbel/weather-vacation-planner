d1a71a71d4b20617bbaca006620c1a83e5d943ed
Rodrigo <rcoto@protonmail.com>
2019-05-24 22:08:18 -0700

    env key is working properly

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+require("dotenv").config();
+const DARKSKY_API_KEY = process.env.DARK_SKY_API;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.send(`${DARKSKY_API_KEY}`);
 });
 
 module.exports = router;
