f487362c6ff56c0546ba253b96552676d2eeb9a9
Rodrigo <rcoto@protonmail.com>
2019-05-25 21:58:29 -0700

    Fixed: .env now only works in development mode

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
-require("dotenv").config();
-const DARKSKY_API_KEY = process.env.DARK_SKY_API;
 const axios = require("axios");
-const path = require("path");
 
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, "react-frontend/build")));
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const DARKSKY_API_KEY = process.env.DARK_SKY_API;
+
+// Makes a request to the DarkSky API and returns info object
 
 router.post("/weather", (req, res) => {
   const params = req.body;
