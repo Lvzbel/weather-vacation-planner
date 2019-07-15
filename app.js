2c1b10fddb978bf7bb35a85640d25e2a13c4c9a6
Rodrigo <rcoto@protonmail.com>
2019-07-15 00:14:16 -0700

    Fix: changed express.js to route 404 errors to react

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.sendFile(path.resolve(__dirname, "react-frontend/build/index.html"));
 });
 
 // error handler
